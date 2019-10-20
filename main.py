27c5600fbf76cd475652a7191abf8ed822340f8d
TanyaAkulich <akulich.tanya@gmail.com>
2019-10-20 21:59:27 +0300

    Code refactoring

--- main.py
+++ main.py
@@ -4,39 +4,90 @@ from matplotlib.image import imread
 from skimage.util.shape import view_as_blocks
 import pry;
 
-def initial_data_input():
-    print("Enter part's width: ")
-    part_width = int(input())
 
-    print("Enter part's height: ")
-    part_height= int(input())
+class Studying:
+    def __init__(self):
+        self.image_initialize()
+        self.block_initialize()
+        self.error_max = 3000.0
+        self.error_current = self.error_max + 1
+        self.alpha = 0.0007
+        self.epoch = 0
 
-    return part_width, part_height
+    def study(self):
+        self.set_neural_numbers_on_second_layer()
+        self.set_neural_layers(self.pixels_number())
+        block_form = self.image.to_blocks(self.block.width, self.block.height)
+
+    def image_initialize(self):
+        self.image = Image('simple.png')
+        self.image_array = np.array(self.image)
+
+    def block_initialize(self):
+        block_width, block_height = self.block_data_input()
+        self.block = Block(block_width, block_height)
+
+    def block_data_input(self):
+        print("Enter block's width: ")
+        block_width = int(input())
+
+        print("Enter block's height: ")
+        block_height= int(input())
+
+        return block_width, block_height
+
+    def pixels_number(self):
+        return self.image.width * self.image.height * 3
+
+    def set_neural_numbers_on_second_layer(self):
+        print('Enter number of neurons on the second layer: ')
+        self.neurons_number = int(input())
+
+    def set_neural_layers(self, pixels_number):
+        self.first_layer = np.random.rand(self.number_of_blocks(), self.neurons_number) * 2 - 1
+        self.second_layer = np.copy(self.first_layer).transpose()
+
+    def number_of_blocks(self):
+        return int((self.image.width * self.image.height) / (self.block.width * self.block.height))
 
 
-def image_initialize():
-    image = imread('simple.png')
-    image = (2.0 * image / 1.0) - 1.0
-    a = to_blocks(image)
+class Image:
+    def __init__(self, image_link):
+        self.image_link = image_link
+        self.image_initialize()
 
-def to_blocks(image):
-    image_array = np.array(image)
-    image_height = np.size(image_array, 0)
-    image_width = np.size(image_array, 1)
-    part_width, part_height = initial_data_input()
+    def image_initialize(self):
+        self.image = imread(self.image_link)
+        self.image_array = np.array(self.image)
 
-    blocks = []
-    for py in range(image_width // part_width):
-        x_blocks = []
-        for px in range(image_height // part_height):
-            tmp = []
-            for y in range(part_height):
-                for x in range(part_width):
-                    for k in range(3):
-                        tmp.append(image_array[y][x][k])
-            x_blocks.append(tmp)
-        blocks.append(x_blocks)
-    return np.array(blocks)
+        self.set_image_size()
+
+        return (2.0 * self.image / 1.0) - 1.0
+
+    def set_image_size(self):
+        self.width = np.size(self.image_array, 1)
+        self.height = np.size(self.image_array, 0)
+
+    def to_blocks(self, block_width, block_height):
+        blocks = []
+        for py in range(self.width // block_width):
+            x_blocks = []
+            for px in range(self.height // block_height):
+                tmp = []
+                for y in range(block_height):
+                    for x in range(block_width):
+                        for k in range(3):
+                            tmp.append(self.image_array[y][x][k])
+                x_blocks.append(tmp)
+            blocks.append(x_blocks)
+        return np.array(blocks)
 
 
-image_initialize()
+class Block:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+
+
+Studying().study()
