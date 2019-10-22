6b2b15a9af3b9011a15a0c62fe52c8fb950c3cfc
TanyaAkulich <akulich.tanya@gmail.com>
2019-10-22 23:38:04 +0300

    WIP lerning

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ class Studying:
     def __init__(self):
         self.image_initialize()
         self.block_initialize()
-        self.error_max = 3000.0
+        self.error_max = 0
         self.error_current = self.error_max + 1
-        self.alpha = 0.0007
         self.epoch = 0
+        self.y_values = []
 
     def study(self):
         self.set_neural_numbers_on_second_layer()
         self.set_neural_layers(self.pixels_number())
         block_form = self.image.to_blocks(self.block.width, self.block.height)
+        for block in range(self.number_of_blocks()):
+            self.modify_layer_weight(block_form[block])
 
     def image_initialize(self):
         self.image = Image('simple.png')
@@ -42,6 +44,7 @@ class Studying:
     def set_neural_numbers_on_second_layer(self):
         print('Enter number of neurons on the second layer: ')
         self.neurons_number = int(input())
+        self.error_max = 0.1 * self.neurons_number
 
     def set_neural_layers(self, pixels_number):
         self.first_layer = np.random.rand(self.number_of_blocks(), self.neurons_number) * 2 - 1
@@ -49,6 +52,28 @@ class Studying:
 
     def number_of_blocks(self):
         return int((self.image.width * self.image.height) / (self.block.width * self.block.height))
+
+    def modify_layer_weight(self, block):
+        y = block @ self.first_layer
+        self.y_values.append(y)
+
+        block_on_second_layer = y @ self.second_layer
+        delta = block_on_second_layer - block
+
+        self.learn_neurons_on_first_layer(block, delta)
+        self.adjustment_weight_on_second_layer(y, delta)
+
+    def adjustment_weight_on_second_layer(self, y, delta):
+        self.second_layer -= self.learning_ratio_for_adjustment_weight(y) * y.transpose() * delta
+
+    def learning_ratio_for_adjustment_weight(self, y):
+        1 / sum(y ** 2 for y in self.y_values)
+
+    def learning_ratio_for_learning(self, block):
+        1 / sum(x ** 2 for x in block)
+
+    def learn_neurons_on_first_layer(self, block, delta):
+        self.first_layer -= self.learning_ratio_for_learning(block) * block.transpose() * delta * self.second_layer.transpose()
 
 
 class Image:
