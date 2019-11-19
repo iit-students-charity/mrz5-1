a51949aaaea907e3a0e8982ef28fc51cf4044ea4
TanyaAkulich <akulich.tanya@gmail.com>
2019-11-20 01:36:00 +0300

    Finished lab

--- main.py
+++ main.py
@@ -1,152 +1,117 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.image import imread
-from skimage.util.shape import view_as_blocks
+import matplotlib.image as mpimg
+
 import pry;
 
 
-class Studying:
-    def __init__(self):
-        self.image_initialize()
-        self.block_initialize()
-        self.error_max = 10000
-        self.current_error = self.error_max + 1
-        self.max_alpha = 0.01
-        self.epoch = 0
-        self.y_values = []
-
-    def study(self):
-        # self.set_neural_numbers_on_second_layer()
-        # self.set_neural_layers(self.pixels_number())
-        block_form = self.image.to_blocks(self.block.width, self.block.height)
-        # while self.current_error > 10000:
-        #     self.current_error = 0
-        #     self.epoch += 1
-
-        #     for block in range(np.size(block_form, 0)):
-        #         y = np.matmul(block_form[block], self.first_layer)
-        #         block_on_second_layer = np.matmul(y, self.second_layer)
-        #         delta = np.subtract(block_on_second_layer, block_form[block])
-        #         self.modify_layer_weight(delta[block])
-
-        #         self.learn_neurons_on_first_layer(block_form[block], delta)
-        #         self.adjustment_weight_on_second_layer(y, delta)
-
-        #     print('Epoch ', self.epoch, '   ', 'error ', self.current_error)
-
-        self.show_image(block_form)
-
-    def show_image(self, blocks):
-        self.show_image_from_blocks(self.image.restore_image(blocks, self.block.width, self.block.height))
+def to_blocks(image_height, image_width):
+    blocks = []
+    for i in range(image_height // BLOCK_HEIGHT):
+        for j in range(image_width // BLOCK_WIDTH):
+            block = []
+            for y in range(BLOCK_HEIGHT):
+                for x in range(BLOCK_WIDTH):
+                    for color in range(3):
+                        block.append(ARRAY_IMAGE[i * BLOCK_HEIGHT + y, j * BLOCK_WIDTH + x, color])
+            blocks.append(block)
+    return np.array(blocks)
 
 
-    def show_image_from_blocks(self, blocks):
-            plt.imshow(blocks)
-            plt.axis('off')
-            plt.show()
-
-    def image_initialize(self):
-        self.image = Image('simple.png')
-        self.image_array = np.array(self.image)
-
-    def block_initialize(self):
-        block_width, block_height = self.block_data_input()
-        self.block = Block(block_width, block_height)
-
-    def block_data_input(self):
-        print("Enter block's width: ")
-        block_width = int(input())
-
-        print("Enter block's height: ")
-        block_height= int(input())
-
-        return block_width, block_height
-
-    def pixels_number(self):
-        return self.image.width * self.image.height * 3
-
-    def set_neural_numbers_on_second_layer(self):
-        print('Enter number of neurons on the second layer: ')
-        self.neurons_number = int(input())
-        self.error_max = 0.1 * self.neurons_number
-
-    def set_neural_layers(self, pixels_number):
-        self.first_layer = np.random.rand(self.block.width * self.block.height * 3, self.neurons_number) * 2 - 1
-        self.second_layer = np.copy(self.first_layer).transpose()
-
-    def modify_layer_weight(self, delta):
-        for i in range(np.size(delta, 0)):
-            self.current_error += delta[i] * delta[i]
-
-    def adjustment_weight_on_second_layer(self, y, delta):
-        self.second_layer -= self.learning_ratio(y) * y.transpose() @ delta
-
-    def learning_ratio(self, block):
-        elements_sum = sum(np.matmul(element, element) for element in block)
-
-        return self.max_alpha if elements_sum == 0 else (1 / elements_sum)
-
-    def learn_neurons_on_first_layer(self, block, delta):
-        self.first_layer -= self.learning_ratio(block) * np.matmul(np.matmul(block.transpose(), delta), self.second_layer.transpose())
-
-class Image:
-    def __init__(self, image_link):
-        self.image_link = image_link
-        self.image_initialize()
-
-    def image_initialize(self):
-        self.image = imread(self.image_link)
-        self.image_array = np.array(self.image)
-
-        self.set_image_size()
-        return (2.0 * self.image / 1.0) - 1.0
-
-    def set_image_size(self):
-        self.width = np.size(self.image_array, 1)
-        self.height = np.size(self.image_array, 0)
-
-    def to_blocks(self, block_width, block_height):
-        blocks = []
-        for py in range(self.width // block_width):
-            x_blocks = []
-            for px in range(self.height // block_height):
-                tmp = []
-                for y in range(block_height):
-                    for x in range(block_width):
-                        for k in range(3):
-                            tmp.append(self.image_array[py * block_height + y, px * block_width + x, k])
-                x_blocks.append(tmp)
-            blocks.append(x_blocks)
-        self.block_array = np.array(blocks)
-        return self.block_array
-
-    def to_image(self, blocks, block_width, block_height):
-        array = []
-        blocks_in_line = self.width // block_width
-        for i in range(self.height // block_height):
-            for y in range(block_height):
-                line = []
-                for x in range(block_width):
-                    for j in range(blocks_in_line):
-                        pixel = []
-                        for color in range(3):
-                            pixel.append(blocks[i, (y * block_width * 3) + (x * 3) + color])
-                            # pixel.append(blocks[i, (y * block_width * 3) + (x * 3) + color])
-                        line.append(pixel)
-                array.append(line)
-        return np.array(array)
-
-    def restore_image(self, blocks, block_width, block_height):
-        blocks = 1 * (self.to_image(blocks, block_width, block_height) + 1) / 2
-        # pry()
-        return blocks #.reshape(256, 256, 3)
+def to_array(blocks, image_height, image_width):
+    array = []
+    blocks_in_line = image_width // BLOCK_WIDTH
+    for i in range(image_height // BLOCK_HEIGHT):
+        for y in range(BLOCK_HEIGHT):
+            line = []
+            for j in range(blocks_in_line):
+                for x in range(BLOCK_WIDTH):
+                    pixel = []
+                    for color in range(3):
+                        pixel.append(blocks[i * blocks_in_line + j, (y * BLOCK_WIDTH * 3) + (x * 3) + color])
+                    line.append(pixel)
+            array.append(line)
+    return np.array(array)
 
 
-class Block:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+def show(array_image):
+    read_image = 1 * (array_image + 1) / 2
+    plt.axis('off')
+    plt.imshow(read_image)
+    plt.show()
 
 
+def read_image():
+    image = mpimg.imread("simple.png")
+    image = (2.0 * image / 1.0) - 1.0
+    return np.array(image)
 
-Studying().study()
+
+def image_size():
+    return np.size(ARRAY_IMAGE, 0), np.size(ARRAY_IMAGE, 1)
+
+
+def set_layers():
+    first_layer = np.random.rand(INPUT_LAYER_SIZE, HIDDEN_LAYER_SIZE) * 2 - 1
+    temp = np.copy(first_layer)
+    second_layer = temp.transpose()
+    return first_layer, second_layer
+
+
+def alpha(y):
+    elements_sum = sum(np.matmul(element, element) for element in y)
+    return MAX_ALPHA if elements_sum == 0 else (1 / elements_sum)
+
+def study():
+    current_error = ERROR_MAX + 1
+    epoch = 0
+
+    first_layer, second_layer = set_layers()
+
+    while current_error > ERROR_MAX:
+        current_error = 0
+        epoch += 1
+        for block in blocks():
+            y = block @ first_layer
+            x1 = y @ second_layer
+            delta = x1 - block
+            first_layer -= alpha(y) * np.matmul(np.matmul(block.transpose(), delta), second_layer.transpose())
+            second_layer -= alpha(y) * np.matmul(y.transpose(), delta)
+            error = (delta * delta).sum()
+            current_error += error
+        print('Epoch ', epoch, '   ', 'errors ', current_error)
+    return first_layer, second_layer
+
+
+def blocks():
+    image_height, image_width = image_size()
+    return to_blocks(image_height, image_width).reshape(number_of_blocks(), 1, INPUT_LAYER_SIZE)
+
+
+def number_of_blocks():
+    image_height, image_width = image_size()
+    return int((image_height * image_width) / (BLOCK_HEIGHT * BLOCK_WIDTH))
+
+
+def compres():
+    image_height, image_width = image_size()
+    first_layer, second_layer = study()
+
+    result = []
+    for block in blocks():
+        result.append(block.dot(first_layer).dot(second_layer))
+    result = np.array(result)
+
+    show(ARRAY_IMAGE)
+    show(to_array(result.reshape(number_of_blocks(), INPUT_LAYER_SIZE), image_height, image_width))
+
+
+BLOCK_HEIGHT = 4
+BLOCK_WIDTH = 4
+HIDDEN_LAYER_SIZE = 16
+INPUT_LAYER_SIZE = BLOCK_HEIGHT * BLOCK_HEIGHT * 3
+ERROR_MAX = 10000.0
+ARRAY_IMAGE = read_image()
+MAX_ALPHA = 0.0007
+
+compres()
